9da28201dca89d2f5200b0ba0db906f727d14ee3
testtff <516353433@qq.com>
2023-03-01 17:28:10 +0800

    Update mattkaydiary.py

--- mattkaydiary.py
+++ mattkaydiary.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
                                             dest_path='./mattkaydiary/{}.yaml'.format(datetime.datetime.now(timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M')),
                                             showsize=True, overwrite=True)
 
-        gdd.download_file_from_google_drive(file_id=ids[1],dest_path='./newYaml/mattkaydiary1.yaml',showsize=True, overwrite=True)
+        gdd.download_file_from_google_drive(file_id=ids[1],dest_path='./newYaml/mattkaydiary.yaml',showsize=True, overwrite=True)
         print("网站爬取成功")
         # requests.get('https://api.day.app/3TKmw24emfnWtLN6xyDaW9/网站爬取成功{}'.format(
         #     datetime.datetime.now(timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M')))
