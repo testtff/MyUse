96a8a4eb46ab60d08904712d6cbbada51c08b247
testtff <516353433@qq.com>
2023-03-01 17:37:17 +0800

    Update mattkaydiary.py

--- mattkaydiary.py
+++ mattkaydiary.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     url = "https://www.mattkaydiary.com/search/label/vpn"
     urls = get_urls(url)
     ids = get_ggid(urls[1])
-    os.remove("./newYaml/mattkaydiary1.yaml")
+    #os.remove("./newYaml/mattkaydiary1.yaml")
     if len(ids) == 2:
         gdd.download_file_from_google_drive(file_id=ids[1],
                                             dest_path='./mattkaydiary/{}.yaml'.format(datetime.datetime.now(timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M')),
